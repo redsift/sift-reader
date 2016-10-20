0b1ea4f6f20bd7a34cb749d8952b3dbad9351998
Christos Vontas <christos@redsift.io>
2016-10-20 19:34:44 +0100

    added hero and slider

--- frontend/src/scripts/view.js
+++ frontend/src/scripts/view.js
@@ -2,12 +2,16 @@
  * Counter Sift. Frontend view entry point.
  */
 import { SiftView, registerSiftView } from '@redsift/sift-sdk-web';
+import { Sliders } from '@redsift/ui-rs-core';
+import '@redsift/ui-rs-hero';
 
 export default class MyView extends SiftView {
   constructor() {
     // You have to call the super() method to initialize the base class.
     super();
+    Sliders.initAllRanges();
     this.controller.subscribe('counts', this.onCounts.bind(this));
+    this.sliderHandler();
   }
 
   // for more info: https://docs.redsift.com/docs/client-code-siftview
