3fbdb1f1704cd43424bc625c6f294c50a1a8085c
Christos Vontas <christos@redsift.io>
2016-10-26 18:32:45 +0100

    casting to number

--- frontend/src/scripts/lib/util.js
+++ frontend/src/scripts/lib/util.js
@@ -1,12 +1,12 @@
 export function bucketing(w){
-  return Math.round(w*60) <= 10 ? 1
-      : Math.round(w*60) <= 90 ? 2
-      : w <= 5 ? 3
+  return Math.round(+w * 60) <= 10 ? 1
+      : Math.round(+w * 60) <= 90 ? 2
+      : +w <= 5 ? 3
       : 4;
 }
 
 export function tooltip(v) {
   return bucketing(v) === 1 ? '< 10 sec read'
     : bucketing(v) === 2 ? '< 1 min read'
-    : `${Math.round(v)} min read`;
+    : `${Math.round(+v)} min read`;
 }
